97e8c9a243f4cacaa0cb25ee6cf4ed6999589990
Gabriel Pelouze <gabriel@pelouze.net>
2018-09-14 02:55:45 +0200

    add support for slot data

--- eis_pointing/eis_aia_emission.py
+++ eis_pointing/eis_aia_emission.py
@@ -5,6 +5,7 @@ import warnings
 import numpy as np
 
 from .utils import eis
+from .utils import num
 
 def compute(windata, wl0, wl_width):
     ''' Compute synthetic emission for a given AIA band using EIS data.
@@ -19,18 +20,34 @@ def compute(windata, wl0, wl_width):
     wl_width : float
         The width wavelength of the integration domain, in Ångström.
     '''
-    raster = windata.int.copy()
-    missing_places = (raster == windata.missing)
-    raster[missing_places] = np.nan
-    raster /= windata.exposure_time.reshape(-1, 1)
-    wvl_min = wl0 - wl_width
-    wvl_max = wl0 + wl_width
-    i_min = np.argmin(np.abs(windata.wvl - wvl_min))
-    i_max = np.argmin(np.abs(windata.wvl - wvl_max))
-    raster = raster[:, :, i_min:i_max+1]
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', category=RuntimeWarning)
-        intensity = np.nanmean(raster, axis=-1)
+
+    slot = windata.hdr.slit_id[0].decode() in ('40"', '266"')
+    if slot:
+        # slot windata don't have a .missing tag
+        windata.missing = -100
+        windata.exposure_time = np.array([float(et)
+            for et in windata.exposure_time])
+        t_ref = num.parse_date(windata.hdr['date_obs'][0])
+        t_abs = num.parse_date(windata.time_ccsds)
+        windata.time = num.total_seconds(t_abs - t_ref)
+    else:
+        windata.exposure_time = windata.exposure_time.reshape(-1, 1)
+
+    intensity = windata.int.copy()
+    missing_places = (intensity == windata.missing)
+    intensity[missing_places] = np.nan
+    intensity /= windata.exposure_time
+
+    if not slot:
+        wvl_min = wl0 - wl_width
+        wvl_max = wl0 + wl_width
+        i_min = np.argmin(np.abs(windata.wvl - wvl_min))
+        i_max = np.argmin(np.abs(windata.wvl - wvl_max))
+        intensity = intensity[:, :, i_min:i_max+1]
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', category=RuntimeWarning)
+            intensity = np.nanmean(intensity, axis=-1)
+
     pointing = eis.EISPointing.from_windata(windata, use_wvl=False)
     data = eis.EISData(intensity, pointing)
     return data
--- eis_pointing/export_windata.pro
+++ eis_pointing/export_windata.pro
@@ -7,6 +7,10 @@ for i=0, nfiles-1 do begin
   wd_file = wd_files[i]
   l0_file = l0_files[i]
   wd = eis_getwindata(l0_file, wl0, /refill)
+  if (wd.hdr.slit_id eq '40"') or (wd.hdr.slit_id eq '266"') then begin
+    print, 'Reading slot.'
+    eis_readslot, l0_file, wd, win=wl0, /estimate
+  endif
   save, wd, filename=wd_file
 endfor
 
