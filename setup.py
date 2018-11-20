94e50d29307fab0658b211370a16268ddc4b0f68
Gabriel Pelouze <gabriel.pelouze@ias.u-psud.fr>
2018-11-20 14:09:19 +0100

    bump to v2018.11.20

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ entry_points = {
 
 setuptools.setup(
     name='eis_pointing',
-    version='2018.10.30',
+    version='2018.11.20',
     author='Gabriel Pelouze',
     author_email='gabriel.pelouze@ias.u-psud.fr',
     description='Tools to correct the pointing of Hinode/EIS',
